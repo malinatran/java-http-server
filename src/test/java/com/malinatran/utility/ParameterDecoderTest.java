3d7e047f93af058b3f967b1aeb8e8cbdbf70b43c
malinatran <tran.malina@gmail.com>
2016-11-04 14:12:41 -0700

    Change utility classes to have static methods

--- src/test/java/com/malinatran/utility/ParameterDecoderTest.java
+++ src/test/java/com/malinatran/utility/ParameterDecoderTest.java
@@ -1,29 +1,22 @@
 package com.malinatran.utility;
 
-import org.junit.Before;
 import org.junit.Test;
 import java.util.Map;
 import static org.junit.Assert.*;
 
 public class ParameterDecoderTest {
 
-    private ParameterDecoder decoder;
     private String path;
     private String expected;
     private String actual;
-
-    @Before
-    public void setUp() {
-        decoder = new ParameterDecoder();
-    }
 
     @Test
     public void decodeTextModifiesDecoderMapping() {
         path = "/parameters?hi%there";
 
-        decoder.decodeText(path);
+        ParameterDecoder.decodeText(path);
 
-        Map<String, String> map = decoder.getEncodedCharacters();
+        Map<String, String> map = ParameterDecoder.getEncodedCharacters();
         assertTrue(map.containsKey("%2B"));
         assertEquals("+", map.get("%2B"));
     }
@@ -33,7 +26,7 @@ public class ParameterDecoderTest {
         path = "/parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff";
         expected = "variable_1 = Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?\nvariable_2 = stuff\n";
 
-        actual = decoder.decodeText(path);
+        actual = ParameterDecoder.decodeText(path);
 
         assertEquals(expected, actual);
     }
@@ -43,7 +36,7 @@ public class ParameterDecoderTest {
         path = "/hello?is%20anyone%20out%20there%3F";
         expected = "";
 
-        actual = decoder.decodeText(path);
+        actual = ParameterDecoder.decodeText(path);
 
         assertEquals(expected, actual);
     }
