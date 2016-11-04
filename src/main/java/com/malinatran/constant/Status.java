57ce031e65055b7e1f2b03711c95af1b1d499c71
malinatran <tran.malina@gmail.com>
2016-11-03 22:22:07 -0700

    Remove comments/unnecessary lines of code, add JPG as acceptable file ext

--- src/main/java/com/malinatran/constant/Status.java
+++ src/main/java/com/malinatran/constant/Status.java
@@ -6,7 +6,6 @@ public class Status {
     public static final String NO_CONTENT = "204 No Content";
     public static final String PARTIAL_CONTENT = "206 Partial Content";
     public static final String FOUND = "302 Found";
-    public static final String NOT_MODIFIED = "304 Not Modified";
     public static final String UNAUTHORIZED = "401 Unauthorized";
     public static final String NOT_FOUND = "404 Not Found";
     public static final String METHOD_NOT_ALLOWED = "405 Method Not Allowed";
